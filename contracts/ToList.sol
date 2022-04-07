1e74b6adc420bf8d85d57da2377c95a911b5339a
Luiz Gustavo Graupner Godoy <luiz.graupner@gmail.com>
2022-04-07 06:20:57 -0300

    feat: updated TodoList contract to create tasks

--- contracts/ToList.sol
+++ contracts/ToList.sol
@@ -2,4 +2,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string description;
+        bool complete;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check if it works");
+    }
+
+    function createTask(string memory _description) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _description, false);
+    }
 }
